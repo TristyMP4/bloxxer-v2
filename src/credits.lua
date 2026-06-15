6fa48474e5bf875b6e571fd3b81cf2418cca95fc
TristyMP4 <tristymp4@gmail.com>
2026-06-15 19:11:57 +0100

    Removed from the UI loader for easier modification
    Makes modification easier without having to reobfuscate every single fuckin time

---
+++ src/credits.lua
@@ -0,0 +1,10 @@
+local Tab = ...
+Tab:AddParagraph({
+    Title = "Developer",
+    Content = "bloxxer101104 / trizzy"
+})
+
+Tab:AddParagraph({
+    Title = "Contributor",
+    Content = "Team Bloxxer"
+})
